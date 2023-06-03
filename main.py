2e3090249ac27f1e2efe7bd11348370e0416e7f8
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-04 01:29:53 +0300

    add func(difference_two_numbers) in main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from funcs import sum_two_numbers
+from funcs import sum_two_numbers, difference_two_numbers
+
 num_1 = 10
 num_2 = 5
 
 print(sum_two_numbers(num_1, num_2))
+
+print(difference_two_numbers(num_1, num_2))
